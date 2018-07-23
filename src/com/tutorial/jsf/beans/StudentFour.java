491c966a0cb7308f1e0236e58c7136e0434b5520
Hessam <hessam.modaberi@gmail.com>
2018-07-24 02:06:03 +0430

    checkbox

---
+++ src/com/tutorial/jsf/beans/StudentFour.java
@@ -0,0 +1,38 @@
+package com.tutorial.jsf.beans;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class StudentFour {
+    private String firstName;
+    private String lastName;
+
+    private String favoriteLanguages[];
+
+    public StudentFour() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String[] getFavoriteLanguages() {
+        return favoriteLanguages;
+    }
+
+    public void setFavoriteLanguages(String[] favoriteLanguages) {
+        this.favoriteLanguages = favoriteLanguages;
+    }
+}
