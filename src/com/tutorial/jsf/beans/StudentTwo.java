fccadded47028a72643cd670a3a04830ed3df14c
Hessam <hessam.modaberi@gmail.com>
2018-07-24 01:15:34 +0430

    drop-down list

---
+++ src/com/tutorial/jsf/beans/StudentTwo.java
@@ -0,0 +1,37 @@
+package com.tutorial.jsf.beans;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class StudentTwo {
+    private String firstName;
+    private String lastName;
+    private String country;
+
+    public StudentTwo() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
