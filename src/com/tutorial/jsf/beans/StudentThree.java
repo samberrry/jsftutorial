2de673d3726de83c9b85b0aac142a5bdcbd34e35
Hessam <hessam.modaberi@gmail.com>
2018-07-24 01:35:26 +0430

    radio btn

---
+++ src/com/tutorial/jsf/beans/StudentThree.java
@@ -0,0 +1,64 @@
+package com.tutorial.jsf.beans;
+
+import javax.faces.bean.ManagedBean;
+import java.util.ArrayList;
+import java.util.List;
+
+@ManagedBean
+public class StudentThree {
+    private String firstName;
+    private String lastName;
+    private String country;
+    List<String> countryOptions;
+    private String favoriteLanguage;
+
+    public StudentThree() {
+        countryOptions = new ArrayList<>();
+
+        countryOptions.add("Iran");
+        countryOptions.add("Iraq");
+        countryOptions.add("United States");
+        countryOptions.add("Turkey");
+        countryOptions.add("United Kingdom");
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public List<String> getCountryOptions() {
+        return countryOptions;
+    }
+
+    public void setCountryOptions(List<String> countryOptions) {
+        this.countryOptions = countryOptions;
+    }
+
+    public String getFavoriteLanguage() {
+        return favoriteLanguage;
+    }
+
+    public void setFavoriteLanguage(String favoriteLanguage) {
+        this.favoriteLanguage = favoriteLanguage;
+    }
+}
